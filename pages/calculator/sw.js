919f59a56ae574d029f47ffc88a577e918b53c59
Michael Toner <22949655+mtoner23@users.noreply.github.com>
2025-04-02 23:13:30 -0500

    fix calculator to load inline on my usual page

--- pages/calculator/sw.js
+++ pages/calculator/sw.js
@@ -3,7 +3,7 @@ var filesToCache = [
     './',
     './index.html',
     './calculator.js',
-    './calculator.wasm',
+    './calculator_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
